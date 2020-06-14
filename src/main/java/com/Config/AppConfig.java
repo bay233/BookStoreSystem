ab88377ca10c781ddc6f66161460e9d17fdfdbe6
bay233 <baijiahui233@qq.com>
2020-06-14 14:03:45 +0800

    柏家辉 修改了 数据库连接密码

--- src/main/java/com/Config/AppConfig.java
+++ src/main/java/com/Config/AppConfig.java
@@ -19,7 +19,7 @@ public class AppConfig {
 		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/bss?serverTimezone=UTC");
 		dataSource.setUsername("root");
-		dataSource.setPassword("367960");
+		dataSource.setPassword("a");
 		return dataSource;
 	}
 
